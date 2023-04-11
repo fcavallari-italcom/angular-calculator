dda8367fc0c5af92c60c4ec8726b9e8aab3ac874
Christian de la Fuente <christian.delafuente@gmail.com>
2023-04-11 17:28:59 +0200

    Modifica giorno 11

--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -8,7 +8,37 @@ import { Component, OnInit } from '@angular/core';
 export class CalculatorComponent implements OnInit {
 
   textToShow:string='';
-  constructor() { }
+  sum:number;
+  sos:number;
+  mol:number;
+  div:number;
+  uno:number;
+  due:number;
+  tre:number;
+  quattro:number;
+  cinque:number;
+  sei:number;
+  sette:number;
+  otto:number;
+  nove:number;
+  zero:number;
+  constructor() {
+    this.sum=0;
+    this.sos=0;
+    this.mol=0;
+    this.div=0;
+
+    this.uno=1;
+    this.due=2;
+    this.tre=3;
+    this.quattro=4;
+    this.cinque=5;
+    this.sei=6;
+    this.sette=7;
+    this.otto=8;
+    this.nove=9;
+    this.zero=0;
+   }
 
   ngOnInit(): void {
   }
